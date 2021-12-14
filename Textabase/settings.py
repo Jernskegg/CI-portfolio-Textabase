ef9c8b883587215cc9089e0d546b934dbad2e826
Jernskegg <casper.hille@outlook.com>
2021-12-14 07:55:09 +0100

    Build: Add Procfile, diectories and allowed hosts

--- Textabase/settings.py
+++ Textabase/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_key')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://textabase.herokuapp.com/', 'localhost']
 
 
 # Application definition
