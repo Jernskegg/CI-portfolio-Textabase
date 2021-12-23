a667fcec8ca0b6d655c2e96841f5541d74ab490b
Jernskegg <casper.hille@outlook.com>
2021-12-23 01:45:02 +0100

    Build: Add admin view for threads class

--- forum/admin.py
+++ forum/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import thread
 # Register your models here.
 
+
+class threadAdmin(admin.ModelAdmin):
+
+    prepopulated_fields = {'slug': ('title',)}
+    exclude = ('upvote',)
+
+
+admin.site.register(thread, threadAdmin)
